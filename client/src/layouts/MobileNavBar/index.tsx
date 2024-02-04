8b07ba18901d160de4e546d33f218609aa3a2362
Tasssiooo <tassioribeirofontes@gmail.com>
2024-02-04 19:41:40 -0300

    This navigation bar doesn't appear in the main page

--- client/src/layouts/MobileNavBar/index.tsx
+++ client/src/layouts/MobileNavBar/index.tsx
@@ -2,10 +2,13 @@ import Logotype from "./components/Logotype";
 import NavDrawer from "./components/NavDrawer";
 
 export default function MobileNavBar() {
-  return (
-    <div className="lg:hidden flex flex-row justify-between items-center container h-16 border-b">
-      <Logotype />
-      <NavDrawer />
-    </div>
-  );
+  const pathname = window.location.pathname;
+  if (pathname !== "/") {
+    return (
+      <div className="lg:hidden flex flex-row justify-between items-center container h-16 border-b">
+        <Logotype />
+        <NavDrawer />
+      </div>
+    );
+  }
 }
