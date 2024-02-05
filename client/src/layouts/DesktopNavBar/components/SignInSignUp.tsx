feb3c9a62e828ba2b541cb96c82609db3e5a9f20
Tasssiooo <tassioribeirofontes@gmail.com>
2024-02-05 01:06:53 -0300

    Adjusts in classNames

--- client/src/layouts/DesktopNavBar/components/SignInSignUp.tsx
+++ client/src/layouts/DesktopNavBar/components/SignInSignUp.tsx
@@ -1,18 +1,26 @@
 import { Button } from "@/components/ui/button";
 
+import { cn } from "@/lib/utils";
+
 export default function SignInSignUp() {
   const pathname = window.location.pathname;
   const isMainPage = pathname === "/";
 
   return (
     <div
-      className={`flex flex-row justify-center items-center p-2 bg-primary${isMainPage ? "-foreground" : ""} rounded-full gap-1`}
+      className={cn(
+        "flex flex-row justify-center items-center p-2 rounded-full gap-1",
+        isMainPage ? "bg-primary-foreground" : "bg-primary"
+      )}
     >
       <Button
         type="button"
         size="sm"
         variant="ghost"
-        className={`text-primary${isMainPage ? "" : "-foreground"} hover:text-primary-foreground hover:bg-[#942a2a] rounded-full`}
+        className={cn(
+          "hover:text-primary-foreground hover:bg-[#942a2a] rounded-full",
+          isMainPage ? "text-primary" : "text-primary-foreground"
+        )}
       >
         Sign In
       </Button>
